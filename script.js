5e5b9849fbe4e6f3f5288b0bf26401eed3f8dc2d
Kimberly Carver <78159772+CarverKR@users.noreply.github.com>
2021-02-14 12:34:56 -0600

    Create script.js

---
+++ script.js
@@ -0,0 +1,25 @@
+require(["esri/views/MapView", "esri/WebMap"], function(MapView, WebMap) {
+        /************************************************************
+         * Creates a new WebMap instance. A WebMap must reference
+         * a PortalItem ID that represents a WebMap saved to
+         * arcgis.com or an on-premise portal.
+         *
+         * To load a WebMap from an on-premise portal, set the portal
+         * url with esriConfig.portalUrl.
+         ************************************************************/
+        var webmap = new WebMap({
+          portalItem: {
+            // autocasts as new PortalItem()
+            id: "b0b82d62f0044b1e9d6b22e34c313ee6"
+          }
+        });
+
+        /************************************************************
+         * Set the WebMap instance to the map property in a MapView.
+         ************************************************************/
+        var view = new MapView({
+          map: webmap,
+          container: "viewDiv",
+          zoom: 10
+        });
+      });
